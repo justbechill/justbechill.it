3af1cf49577336ba4d5fc9aa2fe3370e562245b6
Kyle Malling <kmalling16@gmail.com>
2025-09-05 12:04:41 -0700

    Trying root paths please work

--- site/script.js
+++ site/script.js
@@ -2,17 +2,32 @@ function greetUser(name) {
   alert("Hello, " + name + "!");
 }
 
-// FOOTER AND HEADER SHENANIGANS
+// FOOTER AND NAVBAR SHENANIGANS
 var footerString = `
+  <div class="footer">
     <p>
-      All photographs on this website are licensed under a
+      All photographs on this website are under a
       <a
         href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
       >
         CC BY-NC-SA 4.0 License
       </a>
-    </p>`;
+    </p>
+  </div>`;
 
-let footerElement = document.getElementById("footer");
-footerElement.className = "footer";
-footerElement.innerHTML = footerString;
+//INSERT FOOTER AT THE END OF THE DOCUMENT
+document.querySelector("main").innerHTML += footerString;
+
+var navbarString = `
+  <div class="navbar">
+    <img src="/photos/icon.svg" />
+    <div class="navbar-name"><a href="./">justbechill</a></div>
+
+    <a href="/photos">Photos</a>
+    <a href="/music">Music</a>
+    <a href="/movies">Movies</a>
+  </div>`;
+
+//INSERT NAVBAR AT THE START OF THE DOCUMENT
+document.querySelector("main").innerHTML =
+  navbarString + document.querySelector("main").innerHTML;
