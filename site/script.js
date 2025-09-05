38a6c88693016915ae49e6fb9d74c1cc248108d8
Kyle Malling <kmalling16@gmail.com>
2025-09-04 23:00:00 -0700

    Auto footer, license, and shit

--- site/script.js
+++ site/script.js
@@ -1,6 +1,18 @@
-var name = "Kyle"
-number = 5
+function greetUser(name) {
+  alert("Hello, " + name + "!");
+}
 
-document.getElementById('box').addEventListener('click', function(){
-    alert('Alert test')
-})
+// FOOTER AND HEADER SHENANIGANS
+var footerString = `
+    <p>
+      All photographs on this website are licensed under a
+      <a
+        href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+      >
+        CC BY-NC-SA 4.0 License
+      </a>
+    </p>`;
+
+let footerElement = document.getElementById("footer");
+footerElement.className = "footer";
+footerElement.innerHTML = footerString;
